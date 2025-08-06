25727b6bbb10b9cf9cc437d35f0bcc62b5d94eb5
CrowCode32 <crowsofmoye@gmail.com>
2025-08-06 13:42:12 -0500

    cameraController: created sens, vertical min/max and invert y fields. Locked and turned off visibility for cursor.

---
+++ Assets/Scripts/cameraController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+    [SerializeField] int sens;
+    [SerializeField] int lockVertMin, lockVertMax;
+    [SerializeField] bool invertY;
+
+    float rotX;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
