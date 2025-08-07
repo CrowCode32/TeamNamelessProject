3a09ad05b18c8a0bee0709500a90a98b71437072
Andrew Zito <andrewzito2005@gmail.com>
2025-08-06 19:57:30 -0600

    Player Movement wsda

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Playeryer : MonoBehaviour
+{
+    [SerializeField] CharacterController controller;
+    [SerializeField] int speed;
+
+    Vector3 moveDir;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDir = (Input.GetAxis("Horizontal") * transform.right) +
+                (Input.GetAxis("Vertical") * transform.forward);
+
+        controller.Move(moveDir * speed * Time.deltaTime);
+
+    }
+}
