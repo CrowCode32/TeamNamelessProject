604bdf3e1aa2842c33d5d0cc20dd32ce5281e06f
KJMcLain1980 <kevinmclain1980@hotmail.com>
2025-08-11 11:52:27 -0500

    Updated Scene and implemented new EnemyAI

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,80 +2,90 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Speed
-    [SerializeField] CharacterController controller;
-    [SerializeField] int speed;
-    [SerializeField] int sprintMod;
+    // Movement
+    [SerializeField] private CharacterController controller;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float sprintMod = 1.5f;
 
-    // Jump
-    [SerializeField] int jumpSpeed;
-    [SerializeField] int JumpMax;
-    [SerializeField] int gravity;
+    // Jumping
+    [SerializeField] private float jumpSpeed = 8f;
+    [SerializeField] private int jumpMax = 2;
+    [SerializeField] private float gravity = 20f;
 
-    Vector3 moveDir;
-    Vector3 playerVel;
+    private Vector3 moveDir;
+    private Vector3 playerVel;
+    private int jumpCount;
+    private bool isSprinting;
 
-    int jumpCount;
+    // Shooting
+    [SerializeField] private float shootDist = 100f;
+    [SerializeField] private LayerMask ignoreLayer;
+    [SerializeField] private int shootDamage;
+    private float shootTimer;
 
-    // Used for later on
-    bool isSprinting;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        movement();
-        sprint();
+        Movement();
+        Sprint();
+
+        if (Input.GetButtonDown("Fire1")) Shoot();
     }
 
-    void movement()
+    void Movement()
     {
         if (controller.isGrounded)
         {
             jumpCount = 0;
-            playerVel = Vector3.zero;
-        }
-        else
-        {
-            playerVel.y -= gravity * Time.deltaTime;
+            playerVel.y = -1f; // Keeps grounded
         }
 
         moveDir = (Input.GetAxis("Horizontal") * transform.right) +
                   (Input.GetAxis("Vertical") * transform.forward);
 
-        controller.Move(moveDir * speed * Time.deltaTime);
+        float currentSpeed = isSprinting ? speed * sprintMod : speed;
+        controller.Move(moveDir * currentSpeed * Time.deltaTime);
 
-        jump();
+        Jump();
 
-        controller.Move(playerVel * Time.deltaTime);
         playerVel.y -= gravity * Time.deltaTime;
+        controller.Move(playerVel * Time.deltaTime);
     }
 
-    void jump()
+    void Jump()
     {
-        if (Input.GetButtonDown("Jump") && jumpCount < JumpMax)
+        if (Input.GetButtonDown("Jump") && jumpCount < jumpMax)
         {
             jumpCount++;
             playerVel.y = jumpSpeed;
         }
     }
 
-    void sprint()
+    void Sprint()
     {
         if (Input.GetButtonDown("Sprint"))
         {
-            speed *= sprintMod;
             isSprinting = true;
         }
         else if (Input.GetButtonUp("Sprint"))
         {
-            speed /= sprintMod;
             isSprinting = false;
+        }
+    }
+
+    void Shoot()
+    {
+        shootTimer = 0;
+        RaycastHit hit;
+
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, shootDist, ~ignoreLayer))
+        {
+            Debug.Log(hit.collider.name);
+
+            IDamage dmg = hit.collider.GetComponent<IDamage>();
+            if (dmg != null)
+            {
+                dmg.TakeDamage(shootDamage);
+            }
         }
     }
 }
