e7ba12fa107b0701ef8836adc01f0fae5d2b1d5b
KJMcLain1980 <kevinmclain1980@hotmail.com>
2025-08-11 12:23:38 -0500

    Enemy Ai Script - Recticle and Shoot Damage

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,28 +1,43 @@
 using UnityEngine;
+using System.Collections;
 
 public class EnemyAI : MonoBehaviour, IDamage
 {
-
+    [SerializeField] Renderer model;
     [SerializeField] int HP;
+
+    Color colorOrig;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        colorOrig = model.material.color;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     public void TakeDamage(int amount)
     {
-        HP -= amount;
-        if(HP <= 0)
+        if (HP > 0)
+
+        {
+            HP -= amount;
+            StartCoroutine(flashRed());
+        }
+        if (HP <= 0)
         {
             Destroy(gameObject);
         }
+    }
+
+    IEnumerator flashRed()
+    {
+        model.material.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        model.material.color = colorOrig;
     }
 }
 
