2adaeb302e97b25b77760dda041e9f7a1794554a
MikazukiAugust <andrewzito2005@gmail.com>
2025-08-22 22:00:30 -0600

    Minimal changes

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour, IDamage // IHeal
+public class PlayerMovement : MonoBehaviour, IDamage, IHeal
 {
     [SerializeField] LayerMask ignoreLayer;
 
