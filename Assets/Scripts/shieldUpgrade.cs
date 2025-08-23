518d51d9b7c1796759da8ef337daa7c6fafe704a
CrowCode32 <crowsofmoye@gmail.com>
2025-08-23 13:24:28 -0500

    Shield Toggle & Charge Introduced
    The shield toggles on when a button is pressed and the player has a shield charge, and won't turn on when the player has no charges. Still working on durability and collision.

---
+++ Assets/Scripts/shieldUpgrade.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class shieldUpgrade : MonoBehaviour
+{
+    [SerializeField] CapsuleCollider shield;
+    [SerializeField] int shieldDurability;
+    [SerializeField] bool shieldCharged;
+    int maxDurability;
+
+    // Update is called once per frame
+    private void Start()
+    {
+        maxDurability = shieldDurability;
+    }
+
+    void Update()
+    {
+        //If the player hasn't picked up the upgrade, return
+        //If the player presses a certain button and the shield has charge, toggle on; else off
+        if (Input.GetButtonDown("Shield") && shieldCharged == true)
+        {
+            shieldDurability = maxDurability;
+            shield.enabled = true;
+            shieldCharged = false;
+        } 
+
+        if(shieldDurability == 0)
+        {
+            shield.enabled = false;
+        }
+        
+    }
+
+    /*private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer == 7)
+        {
+            shieldDurability--;
+        }
+    }*/
+}
