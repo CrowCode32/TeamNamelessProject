a8ed76b62c9e3fa67f23c6d44e67c0933f54e6d0
KJMcLain1980 <kevinmclain1980@hotmail.com>
2025-08-24 04:38:35 -0500

    Update

--- Assets/Scripts/gunStats.cs
+++ Assets/Scripts/gunStats.cs
@@ -2,18 +2,19 @@ using UnityEngine;
 
 [CreateAssetMenu]
 
-public class gunStats : ScriptableObject
+public class GunStats : ScriptableObject
 {
     public GameObject model;
-    [Range(1, 10)] public int shootDamage;
-    [Range(5, 1000)] public int shootDistance;
-    [Range(0.1f, 3)] public float shootRate;
-    public int ammoCur;
-    [Range(5, 50)] public int ammoMax;
+
+   [Range(1,10)] public int shootDamage;
+   [Range(5,1500)] public int shootDistance;
+   [Range(0.1f,3)] public float shootRate;
+
+    public int ammoCurr;
+
+   [Range(5,60)] public int ammoMax;
 
     public ParticleSystem hitEffect;
-    public AudioClip[] shootSound;
-
-    [Range(0, 1)] public float shootVol;
-
+    public AudioClip[] shootSounds;
+   [Range(0,1)] public float shootVol;
 }
