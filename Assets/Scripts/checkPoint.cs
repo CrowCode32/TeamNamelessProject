c17e8fce6ea5df7cf7cdc26eb7a807fadc02fef5
MikazukiAugust <andrewzito2005@gmail.com>
2025-08-27 01:15:08 -0600

    Added respawn and checkpoint code

---
+++ Assets/Scripts/checkPoint.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections;
+
+public class checkPoint : MonoBehaviour
+{
+    [SerializeField] Renderer model;
+
+    Color colorOrig;
+
+    private void Start()
+    {
+        colorOrig = model.material.color;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && gameManager.instance.playerSpawnPos.transform.position != transform.position)
+        {
+            gameManager.instance.playerSpawnPos.transform.position = transform.position;
+            StartCoroutine(checkpointFeedback());
+        }
+    }
+
+    IEnumerator checkpointFeedback()
+    {
+        model.material.color = Color.red;
+        gameManager.instance.checkpointPopup.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        gameManager.instance.checkpointPopup.SetActive(false);
+        model.material.color = colorOrig;
+    }
+}
