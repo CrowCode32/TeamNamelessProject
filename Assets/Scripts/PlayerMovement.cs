ea2729259adefe4ca50e6222afab72593c325a4f
Chaz <awesome030703@gmail.com>
2025-08-27 12:26:21 -0700

    3 guns made
    made all three guns

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -126,13 +126,17 @@ public class PlayerMovement : MonoBehaviour, IDamage, IHeal, IPickup
     void shoot()
     {
         shootTimer = 0;
+        gunList[gunListPos].ammoCur--;
 
         RaycastHit hit;
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, shootDist, ~ignoreLayer))
         {
             Debug.Log(hit.collider.name);
+            Instantiate(gunList[gunListPos].hitEffect, hit.point, Quaternion.identity);
+
 
             IDamage dmg = hit.collider.GetComponent<IDamage>();
+
 
             if(dmg != null)
             {
