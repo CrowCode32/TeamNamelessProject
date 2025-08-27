8ffc33da9f0f5199db45803ccaa674635af7ab7c
CrowCode32 <crowsofmoye@gmail.com>
2025-08-27 18:56:21 -0500

    Gun Troubleshooting
    Troubleshooting gun stuff after merging with Chaz's branch.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour, IDamage, IHeal, IPickup, IReload
+public class PlayerMovement : MonoBehaviour, IDamage, IHeal, IPickups, IReload
 {
     [SerializeField] LayerMask ignoreLayer;
 
